880284400bfde1142be146cdd1df30e2b098f92d
Jake Pitman <jakeseanp@gmail.com>
2024-02-20 20:59:49 +1300

    Add glass

--- app/Experience.tsx
+++ app/Experience.tsx
@@ -2,6 +2,19 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { Environment } from "@react-three/drei";
+import * as THREE from "three";
+
+const glassMaterial = new THREE.MeshPhysicalMaterial({
+  color: "white",
+  metalness: 0,
+  roughness: 0,
+  transmission: 0.9,
+  transparent: true,
+  ior: 1.5,
+  opacity: 0.3,
+  side: THREE.DoubleSide,
+});
 
 export const Experience = () => {
   return (
@@ -9,10 +22,15 @@ export const Experience = () => {
       <color args={["black"]} attach="background" />
       <OrbitControls />
       <ambientLight />
+      <Environment blur={0.1} background={false} preset="city" />
 
       <mesh rotation={[Math.PI * 0.25, Math.PI * 0.25, 0]}>
         <boxGeometry args={[1, 1, 1]} />
-        <meshNormalMaterial />
+        <meshStandardMaterial emissive={[1, 1, 1]} emissiveIntensity={1} />
+      </mesh>
+
+      <mesh material={glassMaterial} position={[0, 0, 1]}>
+        <boxGeometry args={[3, 3, 0.2]} />
       </mesh>
     </Canvas>
   );
